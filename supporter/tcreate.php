1cc9cd341c9aaaf027c6a61937b687a3ee3dc095
Peter Horrack <phorrack@casaria.net>
2017-08-12 14:51:08 -0700

    createSupporterInfo() fixed on change and set default status correctly

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -224,7 +224,7 @@ function createSupporterUserMenu($group_id)
 	global $mysql_users_table, $db, $sg, $ug, $userid, $cookie_name;
 
 	
-		$sql = "select id,user_name,supporter from $mysql_users_table order by user_name asc where supporter = 0";
+		$sql = "select id,user_name,supporter from $mysql_users_table where supporter = 0 order by user_name asc ";
 
 	$result = $db->query($sql);
 

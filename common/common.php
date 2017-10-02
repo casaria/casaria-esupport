e7672ffdaa0865e654f6afb8c2a7e6bb45c2b70d
Peter Horrack <phorrack@casaria.net>
2017-10-02 11:02:47 -0700

    Revert

--- common/common.php
+++ common/common.php
@@ -1092,14 +1092,11 @@ function getRStatus($rank)
 ************************************************************************************************************/
 function getHighestRank($table)
 {
-	global $mysql_tstatus_table, $db;
+	global  $db;
 
-	if($table == $mysql_tstatus_table) {
-        $sql = "select id from $table order by rank desc";
-    }else{
-		$sql = "select id from $table order by rank asc";
-	}
-	
+		 $sql = "select id from $table order by rank desc";
+
+
 	$result = $db->query($sql);
 	$row = $db->fetch_row($result);
 	return $row[0];
@@ -1123,11 +1120,11 @@ function getLowestRank($table)
 	else{
 		$sql = "select id, default_create from $table order by rank desc";
 	}
-	
+
 	$result = $db->query($sql);
-	
+
 	while($row = $db->fetch_array($result)){
-	     
+
 	     if ($row["default_create"]) return $row[0];
 	}
 }
@@ -1147,12 +1144,12 @@ function getHoldRank($table)
 	else{
 		$sql = "select id, default_create from $table order by rank desc";
 	}
-	
+
 	$result = $db->query($sql);
-	
+
 	$row = $db->fetch_array($result);
         return $row[0];
-	
+
 }
 
 /***********************************************************************************************************
@@ -1441,7 +1438,7 @@ function createBillingStatusMenu($flag = 0, $new = 0)
 {
     global $mysql_tBillingStatus_table, $info, $db;
 
-    $sql = "select id,status, default_create from $mysql_tBillingStatus_table order by rank asc";
+    $sql = "select id, status, default_create from $mysql_tBillingStatus_table order by rank asc";
     $result = $db->query($sql, $mysql_tBillingStatus_table);
 
     if($flag == 1)

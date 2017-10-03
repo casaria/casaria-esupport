c2d5aaeb1c202e6e478bc571ac77c02d141d66b4
Peter Horrack <phorrack@casaria.net>
2017-10-03 09:01:38 -0700

    localtime create time

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -79,18 +79,11 @@ if(isset($create)){
 	$emailstatuschange = ($emailstatuschange == "on") ? "On" : "Off";
   
 	$billing_status = "0";
-	$timenow = localtime(time(),TRUE);
-
-    if (($cyear != 0)) {
-        $ctimestamp = mktime($chour, $cminute, 0, $cmonth, $cday, $cyear);
-    } else {
-        $ctimestamp = $timenow;
-    }
-
+    $ctimestamp = mktime($chour, $cminute, 0, $cmonth, $cday, $cyear);
 	
-	$sql = "INSERT into $mysql_tickets_table values(NULL, '$ctimestamp', '$s', '$ugroup_id', '$name',
+	$sql = "INSERT into $mysql_tickets_table values(NULL, $ctimestamp, $sg, $ugroup_id, '$name',
 	 			 $supporter_id, '$priority', '$status', '$billing_status',	'$username', '$email', '$office', '$phone',
-				 '$equipment', '$category', '$platform', '$short', '$description', NULL, 0, '$ctimestamp',
+				 '$equipment', '$category', '$platform', '$short', '$description', NULL, 0, $ctimestamp,
 				 '$emailgroup', '$emailstatuschange', '$emailcc', 0)";
 	
 	

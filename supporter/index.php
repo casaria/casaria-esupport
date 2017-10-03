296d20c67cf41b6294754534b29a7aa0a15116c6
Peter Horrack <phorrack@casaria.net>
2017-10-03 10:27:30 -0700

    localtime create time

--- supporter/index.php
+++ supporter/index.php
@@ -361,15 +361,14 @@ if (isAdministrator($cookie_name) && $awaiting_approval){
 							require "announce.php";
 							break;
 					}
-                                $sql = "SELECT 'id' FROM $mysql_tickets_table ORDER BY 'id' DESC LIMIT 1";
+                                $sql = $sql = 'SELECT "id" FROM $mysql_tickets_table ORDER BY "id" DESC LIMIT 1';
                                 $result = $db->query($sql);
                                 $row = $db->fetch_row($result);
                                 $id = $row[0];
                                 $lastticketid=$id;
 
                     if ($showticket)  echo "Ticket susccesfully created:  TKT# $lasticketid <BR>";
-                    echo "tits";
-						  
+
 					?>
 				
               </TR>

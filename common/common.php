cc06eedfb4f6e65433f4056b08d819f34797aacc
Peter Horrack <phorrack@casaria.net>
2017-10-03 05:00:29 -0700

    Casaria CRM space (cosmetic)

--- common/common.php
+++ common/common.php
@@ -1812,11 +1812,9 @@ function createTicketInfo($flag='allow', $equipmentgroupid = 0)
 						<tr>
 							<td width=20% class=back2 align=right>* '.$lang_equipment.':</td>
 							<td class=back colspan=3><select name=equipment>';  createEquipmentMenu(0,$equipmentgroupid);
-							echo '	</select></td>
-							
-							<td width=20% class=back2 align=right>* \'.$lang_equipment.\':</td>
-							<td class=back colspan=3><select name=equipment>\';  createEquipmentMenu(0,$equipmentgroupid);
-							echo \'	</select></td>			
+							echo '	</select></td><td class=back2 width=100 align=right>* '.$lang_date.':</td>
+							<td class=back colspan=3><select name=date>';  createEquipmentMenu(0,$equipmentgroupid);
+							echo '	</select></td>			
 																
 						</tr>
 						<tr>

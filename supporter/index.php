21751680ed33c03e6632cb45e6c17f4fa974934e
Peter Horrack <phorrack@casaria.net>
2017-10-07 15:49:56 -0700

    CELL

--- supporter/index.php
+++ supporter/index.php
@@ -29,7 +29,7 @@
 			**
 			***************************************************************************************/
 
-rkequire_once "../common/config.php";
+require_once "../common/config.php";
 require_once "../common/$database.class.php";
 require_once "../common/common.php";
 
